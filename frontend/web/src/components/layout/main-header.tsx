c987e2118a736f7c68da141622628826b6a19f48
Huynh Dinh Vu <vu0905531855@gmail.com>
2026-04-25 15:52:15 +0700

    feat: update admin source

--- frontend/web/src/components/layout/main-header.tsx
+++ frontend/web/src/components/layout/main-header.tsx
@@ -56,7 +56,7 @@ const MainHeader: React.FC = () => {
               <div className='w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center'>
                 <GraduationCap className='w-6 h-6 text-white' />
               </div>
-              <span className='text-xl font-bold text-blue-600'>DTUHub</span>
+              <span className='text-xl font-bold text-blue-600'>Educonnect Live</span>
             </div>
             <div className='hidden md:flex items-center bg-gray-100 rounded-full ml-4 relative'>
               <SearchField
